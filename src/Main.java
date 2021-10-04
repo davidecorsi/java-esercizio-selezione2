144578f52bc4a1a15909045c2d36d94f132b4f59
Davide Corsi <davide.corsi@par-tec.it>
2021-10-04 10:45:02 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+ * Scrive un applicazione che calcoli l'importo di una tassa seguendo questa tabella:
+ * 1 - fino a 10000€ la tassa è del 10%
+ * 2 - fino a 20000€ la tassa è del 10% per i primi 10000€ e del 7% sul resto
+ * 3 - fino a 30000€ la tassa è del 10% per i primi 10000€, del 7% fino a 20000€ e del 5% sul resto
+ * 4 - oltre i 30000€ si aggiunge un ulteriore 3% sulla porzione oltre i 30000€
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
